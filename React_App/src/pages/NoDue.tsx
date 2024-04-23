49ba9fd5ebae41f7bfcb56acd799774e6519d460
LakshmiNarashimmanM <lakshmi_narashimman@agnikul.in>
2024-04-23 12:49:23 +0530

    No Due

--- React_App/src/pages/NoDue.tsx
+++ React_App/src/pages/NoDue.tsx
@@ -6,6 +6,8 @@ import {
   Radio,
   RadioGroup,
   Button,
+  Typography,
+  Grid,
 } from "@mui/material";
 
 interface NoDueProps {
@@ -18,6 +20,11 @@ const NoDue: React.FC<NoDueProps> = ({ darkMode, onCloseDrawer }) => {
     useState<string>("");
   const [idCardValue, setIdCardValue] = useState<string>("");
   const [laptopValue, setLaptopValue] = useState<string>("");
+  const [notebookValue, setNotebookValue] = useState<string>("");
+  const [mobileValue, setMobileValue] = useState<string>("");
+  const [stationeryValue, setStationeryValue] = useState<string>("");
+
+
 
   const SelectOptionFun = (
     event: React.ChangeEvent<HTMLInputElement>,
@@ -28,7 +35,7 @@ const NoDue: React.FC<NoDueProps> = ({ darkMode, onCloseDrawer }) => {
   };
 
   return (
-    <Box>
+    <Box className='nodue_form'>
       <div className="m-4">
         <Box sx={{ display: "flex", alignItems: "center" }}>
           <Box
@@ -49,70 +56,78 @@ const NoDue: React.FC<NoDueProps> = ({ darkMode, onCloseDrawer }) => {
         <br />
         <br />
 
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            marginBottom: "16px",
-            justifyContent: "space-between",
-          }}
-        >
-          <Box sx={{ marginRight: "16px" }}>
-            Have you submitted Official Documents?
-          </Box>
-          <FormControl component="fieldset">
-            <RadioGroup
-              aria-label="officialDocuments"
-              name="officialDocuments"
-              value={officialDocumentsValue}
-              onChange={(event) =>
-                SelectOptionFun(event, setOfficialDocumentsValue)
-              }
-              sx={{ display: "flex", flexDirection: "row", flexWrap: "nowrap" }}
-            >
-              <FormControlLabel
-                value="yes"
-                control={<Radio />}
-                label="Yes"
-                sx={{
-                  color: "#4d8c52",
-                  "& .MuiTypography-root": { fontSize: "14px" },
-                  "&.Mui-checked": { color: "#4D8C52" },
-                }}
-              />
-              <FormControlLabel
-                value="no"
-                control={<Radio />}
-                label="No"
-                sx={{
-                  color: "#4d8c52",
-                  "& .MuiTypography-root": { fontSize: "14px" },
-                  "&.Mui-checked": { color: "red" },
-                }}
-              />
-              <FormControlLabel
-                value="pending"
-                control={<Radio />}
-                label="N/A"
-                sx={{
-                  color: "#4d8c52",
-                  "& .MuiTypography-root": { fontSize: "14px" },
-                  "&.Mui-checked": { color: "#5d5d5d" },
-                }}
-              />
-            </RadioGroup>
-          </FormControl>
-        </Box>
+        <Grid container alignItems="center" spacing={2}  width={{ xs: "140%", sm: "120%", md: "50%" }}
+                marginBottom="16px"textAlign={"right"}>
+          <Grid item xs={4} sm={6} md={6} xl={6} lg={6} display="flex" flexBasis="flex-end" >
+            <Typography>Yes</Typography>
+            <Typography>No</Typography>
 
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            marginBottom: "16px",
-            justifyContent: "space-between",
-          }}
-        >
-          <Box sx={{ marginRight: "16px" }}>Have you submitted ID Card?</Box>
+            <Typography>N/A</Typography>
+
+          </Grid>
+         
+        </Grid>
+        <Grid container alignItems="center" spacing={3} paddingLeft="5vw" width={{ xs: "140%", sm: "120%", md: "110%" }}
+                marginBottom="16px"textAlign={"left"}>
+          <Grid item xs={4} sm={6} md={6} xl={6} lg={6}>
+            <Typography>Have you submitted Official Documents?</Typography>
+          </Grid>
+          <Grid item xs={8} sm={6}>
+            <FormControl component="fieldset">
+              <RadioGroup
+                aria-label="officialDocuments"
+                name="officialDocuments"
+                value={officialDocumentsValue}
+                onChange={(event) =>
+                  SelectOptionFun(event, setOfficialDocumentsValue)
+                }
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+              >
+                <FormControlLabel
+                  value="yes"
+                  control={<Radio />}
+                  label=""
+                  sx={{
+                    color: "#4d8c52",
+                    // "& .MuiTypography-root": { fontSize: "14px" },
+                    "& .Mui-checked": { color: "#4D8C52" },
+                  }}
+                />
+                <FormControlLabel
+                  value="no"
+                  control={<Radio />}
+                  label=""
+                  sx={{
+                    color: "#4d8c52",
+                    // "& .MuiTypography-root": { fontSize: "14px" },
+                    "&.Mui-checked": { color: "#4D8C52" },
+                  }}
+                />
+                <FormControlLabel
+                  value="pending"
+                  control={<Radio />}
+                  label=""
+                  sx={{
+                    color: "#4d8c52",
+                    // "& .MuiTypography-root": { fontSize: "14px" },
+                    "&.Mui-checked": { color: "#4D8C52" },
+                  }}
+                />
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+        </Grid>
+
+        <Grid container alignItems="center" spacing={3} paddingLeft="5vw"  width={{ xs: "140%", sm: "120%", md: "110%" }}
+                marginBottom="16px"textAlign={"left"}>
+          <Grid item xs={4} sm={6} md={6} xl={6} lg={6}>
+            <Typography>Have you submitted ID Cards?</Typography>
+          </Grid>
+          <Grid item xs={8} sm={6}>
           <FormControl component="fieldset">
             <RadioGroup
               aria-label="officialDocuments"
@@ -131,7 +146,7 @@ const NoDue: React.FC<NoDueProps> = ({ darkMode, onCloseDrawer }) => {
                     }}
                   />
                 }
-                label={<span style={{ fontSize: "14px" }}>Yes</span>}
+                label=""
               />
               <FormControlLabel
                 value="no"
@@ -139,11 +154,11 @@ const NoDue: React.FC<NoDueProps> = ({ darkMode, onCloseDrawer }) => {
                   <Radio
                     sx={{
                       color: "#4d8c52",
-                      "&.Mui-checked": { color: "red" },
+                      "&.Mui-checked": { color: "#4D8C52" },
                     }}
                   />
                 }
-                label={<span style={{ fontSize: "14px" }}>No</span>}
+                label=""
               />
               <FormControlLabel
                 value="pending"
@@ -151,25 +166,23 @@ const NoDue: React.FC<NoDueProps> = ({ darkMode, onCloseDrawer }) => {
                   <Radio
                     sx={{
                       color: "#4d8c52",
-                      "&.Mui-checked": { color: "#5d5d5d" },
+                      "&.Mui-checked": { color: "#4D8C52" },
                     }}
                   />
                 }
-                label={<span style={{ fontSize: "14px" }}>N/A</span>}
+                label=""
               />
             </RadioGroup>
           </FormControl>
-        </Box>
+        </Grid>
+        </Grid>
 
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            marginBottom: "16px",
-            justifyContent: "space-between",
-          }}
-        >
-          <Box sx={{ marginRight: "16px" }}>Have you submitted Laptop?</Box>
+        <Grid container alignItems="center" spacing={3} paddingLeft="5vw" width={{ xs: "140%", sm: "120%", md: "110%" }}
+                marginBottom="16px"textAlign={"left"}>
+          <Grid item xs={4} sm={6} md={6} xl={6} lg={6}>
+            <Typography>Have you submitted Laptop?</Typography>
+          </Grid>
+          <Grid item xs={8} sm={6}>
           <FormControl component="fieldset">
             <RadioGroup
               aria-label="officialDocuments"
@@ -188,7 +201,7 @@ const NoDue: React.FC<NoDueProps> = ({ darkMode, onCloseDrawer }) => {
                     }}
                   />
                 }
-                label={<span style={{ fontSize: "14px" }}>Yes</span>}
+                label=""
               />
               <FormControlLabel
                 value="no"
@@ -196,11 +209,11 @@ const NoDue: React.FC<NoDueProps> = ({ darkMode, onCloseDrawer }) => {
                   <Radio
                     sx={{
                       color: "#4d8c52",
-                      "&.Mui-checked": { color: "red" },
+                      "&.Mui-checked": { color: "#4D8C52" },
                     }}
                   />
                 }
-                label={<span style={{ fontSize: "14px" }}>No</span>}
+                label=""
               />
               <FormControlLabel
                 value="pending"
@@ -208,23 +221,186 @@ const NoDue: React.FC<NoDueProps> = ({ darkMode, onCloseDrawer }) => {
                   <Radio
                     sx={{
                       color: "#4d8c52",
-                      "&.Mui-checked": { color: "#5d5d5d" },
+                      "&.Mui-checked": { color: "#4D8C52" },
                     }}
                   />
                 }
-                label={<span style={{ fontSize: "14px" }}>N/A</span>}
+                label=""
               />
             </RadioGroup>
           </FormControl>
-        </Box>
+          </Grid>
+        </Grid>
 
-        <Box display="flex" flexDirection="column" alignItems="center">
+        <Grid container alignItems="center" spacing={3} paddingLeft="5vw"  width={{ xs: "140%", sm: "120%", md: "110%" }}
+                marginBottom="16px"textAlign={"left"}>
+          <Grid item xs={4} sm={6} md={6} xl={6} lg={6}>
+            <Typography>Have you submitted Notebook?</Typography>
+          </Grid>
+          <Grid item xs={8} sm={6}>
+          <FormControl component="fieldset">
+            <RadioGroup
+              aria-label="officialDocuments"
+              name="officialDocuments"
+              value={notebookValue}
+              onChange={(event) => SelectOptionFun(event, setNotebookValue)}
+              sx={{ display: "flex", flexDirection: "row", flexWrap: "nowrap" }}
+            >
+              <FormControlLabel
+                value="yes"
+                control={
+                  <Radio
+                    sx={{
+                      color: "#4d8c52",
+                      "&.Mui-checked": { color: "#4D8C52" },
+                    }}
+                  />
+                }
+                label=""
+              />
+              <FormControlLabel
+                value="no"
+                control={
+                  <Radio
+                    sx={{
+                      color: "#4d8c52",
+                      "&.Mui-checked": { color: "#4D8C52" },
+                    }}
+                  />
+                }
+                label=""
+              />
+              <FormControlLabel
+                value="pending"
+                control={
+                  <Radio
+                    sx={{
+                      color: "#4d8c52",
+                      "&.Mui-checked": { color: "#4D8C52" },
+                    }}
+                  />
+                }
+                label=""
+              />
+            </RadioGroup>
+          </FormControl>
+          </Grid>
+        </Grid>
+        <Grid container alignItems="center" spacing={3} paddingLeft="5vw" width={{ xs: "140%", sm: "120%", md: "110%" }}
+                marginBottom="16px"textAlign={"left"}>
+          <Grid item xs={4} sm={6} md={6} xl={6} lg={6}>
+            <Typography>Have you submitted Mobile?</Typography>
+          </Grid>
+          <Grid item xs={8} sm={6}>
+          <FormControl component="fieldset">
+            <RadioGroup
+              aria-label="officialDocuments"
+              name="officialDocuments"
+              value={mobileValue}
+              onChange={(event) => SelectOptionFun(event, setMobileValue)}
+              sx={{ display: "flex", flexDirection: "row", flexWrap: "nowrap" }}
+            >
+              <FormControlLabel
+                value="yes"
+                control={
+                  <Radio
+                    sx={{
+                      color: "#4d8c52",
+                      "&.Mui-checked": { color: "#4D8C52" },
+                    }}
+                  />
+                }
+                label=""
+              />
+              <FormControlLabel
+                value="no"
+                control={
+                  <Radio
+                    sx={{
+                      color: "#4d8c52",
+                      "&.Mui-checked": { color: "#4D8C52" },
+                    }}
+                  />
+                }
+                label=""
+              />
+              <FormControlLabel
+                value="pending"
+                control={
+                  <Radio
+                    sx={{
+                      color: "#4d8c52",
+                      "&.Mui-checked": { color: "#4D8C52" },
+                    }}
+                  />
+                }
+                label=""
+              />
+            </RadioGroup>
+          </FormControl>
+          </Grid>
+        </Grid>
+        <Grid container alignItems="center" spacing={3} paddingLeft="5vw"  width={{ xs: "140%", sm: "120%", md: "110%" }}
+                marginBottom="16px"textAlign={"left"}>
+          <Grid item xs={4} sm={6} md={6} xl={6} lg={6}>
+            <Typography>Have you submitted Stationery Items?</Typography>
+          </Grid>
+          <Grid item xs={8} sm={6}>
+          <FormControl component="fieldset">
+            <RadioGroup
+              aria-label="officialDocuments"
+              name="officialDocuments"
+              value={stationeryValue}
+              onChange={(event) => SelectOptionFun(event, setStationeryValue)}
+              sx={{ display: "flex", flexDirection: "row", flexWrap: "nowrap" }}
+            >
+              <FormControlLabel
+                value="yes"
+                control={
+                  <Radio
+                    sx={{
+                      color: "#4d8c52",
+                      "&.Mui-checked": { color: "#4D8C52" },
+                    }}
+                  />
+                }
+                label=""
+              />
+              <FormControlLabel
+                value="no"
+                control={
+                  <Radio
+                    sx={{
+                      color: "#4d8c52",
+                      "&.Mui-checked": { color: "#4D8C52" },
+                    }}
+                  />
+                }
+                label=""
+              />
+              <FormControlLabel
+                value="pending"
+                control={
+                  <Radio
+                    sx={{
+                      color: "#4d8c52",
+                      "&.Mui-checked": { color: "#4D8C52" },
+                    }}
+                  />
+                }
+                label=""
+              />
+            </RadioGroup>
+          </FormControl>
+          </Grid>
+        </Grid>
+        <Box display="flex" flexDirection="column"  alignItems="center">
           <Box sx={{ display: "flex" }}>
             <Button className="cancelBtn" onClick={onCloseDrawer}>
               Cancel
             </Button>
 
-            <Button className="saveBtn">Submit</Button>
+            <Button disabled={!officialDocumentsValue||!idCardValue||!notebookValue||!laptopValue||!mobileValue||!stationeryValue} className="saveBtn">Submit</Button>
           </Box>
         </Box>
       </div>
